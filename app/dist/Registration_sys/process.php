221edcde9d78e4ab3dd34f76b138670aaa4d91c8
Mohamed El-Raghy <elraghy8@gmail.com>
2018-11-30 16:07:43 +0200

    Edit process.php to process the input and add it tp the DB

--- app/dist/Registration_sys/process.php
+++ app/dist/Registration_sys/process.php
@@ -1,12 +1,80 @@
 <?php
+session_start();
 
-foreach ($_POST as $key => $value) {
+$username = "";
+$email    = "";
+$errors = []; 
 
-    $value = is_array($value) ? $value : trim($value);
-    if (empty($value) && in_array($key, $required)) {
-        $missing[] = $key;
-        $$key = '';
-    } elseif (in_array($key, $expected)) {
-        $$key = $value;
+$db = mysqli_connect('localhost', 'root', '', 'registration');
+
+// Data comes from register form >> 
+
+if (isset($_POST['reg_usr'])) {
+  
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password1 = mysqli_real_escape_string($db, $_POST['password1']);
+  $password2 = mysqli_real_escape_string($db, $_POST['password2']);
+
+  
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password1)) { array_push($errors, "Password is required"); }
+  if ($password1 != $password2) {
+    array_push($errors, "The two passwords do not match");
+  }
+
+  // Check database to make sure 
+  // a user does not already exist
+  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['username'] === $username) {
+      array_push($errors, "Username already exists");
     }
+
+    if ($user['email'] === $email) {
+      array_push($errors, "email already exists");
+    }
+  }
+
+  if (count($errors) == 0) {
+    $password = md5($password1);
+
+    $query = "INSERT INTO users (username, email, password) 
+              VALUES('$username', '$email', '$password')";
+    mysqli_query($db, $query);
+    $_SESSION['username'] = $username;
+    $_SESSION['success'] = "You are now logged in";
+    header('location: index.php');
+  }
+}
+
+
+// Data comes from login form
+if (isset($_POST['login'])) {
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if (empty($username)) {
+    array_push($errors, "Username is required");
+  }
+  if (empty($password)) {
+    array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+    $password = md5($password);
+    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $results = mysqli_query($db, $query);
+    if (mysqli_num_rows($results) == 1) {
+      $_SESSION['username'] = $username;
+      $_SESSION['success'] = "You are now logged in";
+      header('location: index.php');
+    }else {
+      array_push($errors, "Wrong username/password combination");
+    }
+  }
 }
