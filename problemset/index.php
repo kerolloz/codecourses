7a2243ff30274079d2bd72ce8750f562b82cb5e4
kerolloz <kerolloz@yahoo.com>
2018-12-10 02:59:53 +0200

    remove redundant code for navigation bar, now all link to nav_control.php

--- problemset/index.php
+++ problemset/index.php
@@ -8,23 +8,10 @@
         <script src="/codecourses/scripts/script.js"></script>
     </head>
     <body>
-        <!--Navbar Section-->
-        <nav>
-            <img src="/codecourses/assets/images/codeCourses.png" 
-                 class = "nav-left"
-                 id = "imgMobile">
-            <ul id = "dropdownClick" class = "nav-left">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#problemset">Problem Set</a></li>
-                <li><a href="#roadmap">Road Map</a></li>
-                <li><a href="#contests">Contests</a></li>
-                <li class="nav-right"><a href="#signin">Sign In</a></li>
-                <li class="nav-right"><a href="#signup">Sign Up</a></li>
-                <li class="dropdownIcon"><a href="javascript:void(0);" onclick="dropdownMenu()">&#9776;</a></li>
-            </ul>
-
-        </nav>
-
+        
+        <!--include navigation bar from a preset php file-->
+        <?php require $_SERVER['DOCUMENT_ROOT'] . "/codecourses/navbar_control.php";?>
+        
         <br> <br> <br> <br>
 
         <!-- ProblemSection -->
--- tutorials/index.php
+++ tutorials/index.php
@@ -7,22 +7,11 @@
 
     </head>
     <body>
-        <!--Navbar Section-->
-        <nav>
-            <img src="/codecourses/assets/images/codeCourses.png" 
-                 class = "nav-left"
-                 id = "imgMobile">
-            <ul id = "dropdownClick" class = "nav-left">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#problemset">Problem Set</a></li>
-                <li><a href="#roadmap">Road Map</a></li>
-                <li><a href="#contests">Contests</a></li>
-                <li class="nav-right"><a href="#signin">Sign In</a></li>
-                <li class="nav-right"><a href="#signup">Sign Up</a></li>
-                <li class="dropdownIcon"><a href="javascript:void(0);" onclick="dropdownMenu()">&#9776;</a></li>
-            </ul>
-        </nav>
+       
+        <!--include navigation bar from a preset php file-->
+        <?php require $_SERVER['DOCUMENT_ROOT'] . "/codecourses/navbar_control.php";?>
 
+        
         <!-- Home Section -->
         <div class="row-2">
             <div class="col-6">
