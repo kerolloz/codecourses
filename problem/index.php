26d1b6175f49e2aadcf2b747652a9e035a2af5f3
kerolloz <kerolloz@yahoo.com>
2018-12-10 14:01:57 +0200

    creating home folder, fixing style and script in problem page

--- problem/index.php
+++ problem/index.php
@@ -2,70 +2,109 @@
 <html>
     <head>
         <title>Problem</title>
+
+        <link rel="stylesheet" href = "/codecourses/assets/bootstrap-4.1.3-dist/css/bootstrap.css">
+        <link rel="stylesheet" href="/codecourses/styles/style.css">
         <link rel="stylesheet" href="style.css">
-        <script src="script.js"></script>
+   
     </head>
     <body>
-        
+       
         <!--include navigation bar from a preset php file-->
         <?php require $_SERVER['DOCUMENT_ROOT'] . "/codecourses/navbar_control.php";?>
         
         <br>
-      
-        <!--Problem Section-->
-        <!--Problem pdf -->
-        <embed class="pdfContainer" src="/codecourses/problems_db/<?= $_GET['id'] ?>/<?= $_GET['id'] ?>.pdf"> <!-- src = #problem pdf-->
-        <br><br><br>
-      
-        <!--Select Language Section-->
-        <div class="row-1">
-            <h4>Language:</h4>
-        </div>
-    
-        <div class="user-select">
-            <select name="compiler">
-                Clang++17 Diagnostics
-                <option value="1">GNU G++17 7.3.0</option>
-                <option value="2">PHP 7.0.12</option>
-                <option value="3">Rust 1.26</option>
-                <option value="4">ActiveTcl 8.5</option>
-                <option value="5"> Clang++17 Diagnostics</option>
-                <option value="6">OpenCobol 1.0</option>
-                <option value="7">C# Mono 5</option>
-                <option value="8">D DMD32 v2.079.0</option> 
-                <option value="9">Go 1.11</option>
-                <option value="10">Haskell GHC 7.8.3</option>
-                <option value="11">Java 1.8.0_162</option>
-                <option value="12">Kotlin 1.2</option>
-                <option value="13">JavaScript V8 4.8.0</option>
-                <option value="14">Node.js 6.9.1</option>
-                <option value="15">Microsoft Q#</option>
-                <option value="16">Ruby 2.0.0p645</option>
-                <option value="17">PascalABC.NET 2</option>
-                <option value="18">Python 3.6</option>
-            </select>
-        </div>
-        <br>
-
-        <!--Source Code Section -->
-        <div class="row-3">
-            <h4>Source Code:</h4>
-        </div>
-        
-        <div class="row-4">
-            <form action="###">
-                <textarea  name="sourceCode" rows="30" cols="100" 
-                            placeholder="ENTER YOUR SOLUTION"></textarea>
-                <br>
-                <div class="buttonpos">
-                    <input type="submit" class="button" value="SUBMIT">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <!--Problem pdf -->
+                    <embed class="pdfContainer" src="/codecourses/problems_db/<?= $_GET['id'] ?>/<?= $_GET['id'] ?>.pdf"> <!--src=#problem pdf-->
+                    <br><br><br>
                 </div>
-            </form>
+            </div>
+            
+            <div class="row">
+                <div class="col">
+                    <div class="showsubmit">
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+                          SUBMIT
+                        </button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="demo model" aria-hidden="true">
+                          <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                              <div class="modal-header">
+                                <h4 class="modal-title" id="exampleModalLongTitle">Submit solution</h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                  <span aria-hidden="true">&times;</span>
+                                </button>
+                              </div>
+                                
+                            <div class="modal-body">
+                                <div class="row">
+                                    <div class="col-4">
+                                        <div class="lang"><h5>Language:</h5></div>
+                                    </div>
+                                    <div class="col-8">
+                                        <div class="user-select">
+                                            <select name="compiler">
+                                                Clang++17 Diagnostics
+                                                <option value="1">GNU G++17 7.3.0</option>
+                                                <option value="2">PHP 7.0.12</option>
+                                                <option value="3">Rust 1.26</option>
+                                                <option value="4">ActiveTcl 8.5</option>
+                                                <option value="5"> Clang++17 Diagnostics</option>
+                                                <option value="6">OpenCobol 1.0</option>
+                                                <option value="7">C# Mono 5</option>
+                                                <option value="8">D DMD32 v2.079.0</option> 
+                                                <option value="9">Go 1.11</option>
+                                                <option value="10">Haskell GHC 7.8.3</option>
+                                                <option value="11">Java 1.8.0_162</option>
+                                                <option value="12">Kotlin 1.2</option>
+                                                <option value="13">JavaScript V8 4.8.0</option>
+                                                <option value="14">Node.js 6.9.1</option>
+                                                <option value="15">Microsoft Q#</option>
+                                                <option value="16">Ruby 2.0.0p645</option>
+                                                <option value="17">PascalABC.NET 2</option>
+                                                <option value="18">Python 3.6</option>
+                                            </select>
+                                        </div>
+                                        <br><br><br>
+                                    </div>
+                                </div>
+                                
+                                <div class="row">
+                                    <div class="col-4">
+                                        <!--Source Code Section -->
+                                        <div class="sors"><h5>Source Code:</h5></div>
+                                    </div>
+                                    <div class="col-8">
+                                        <div class="sendSubmit">
+                                            <form action="###">
+                                                <textarea  name="sourceCode" rows="30" cols="30"></textarea>
+                                                <br>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-primary">SUBMIT</button>
+                                </div>
+                            </div>
+                          </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         
-        <!-- Footer Section-->
-        <div id="footer">
-            &copy 2018 CodeCourses.com | All Rights Reserved
-        </div>        
+        <br>
+        <script src="/codecourses/assets/bootstrap-4.1.3-dist/js/jQuery.js"></script>
+        <script src="/codecourses/assets/bootstrap-4.1.3-dist/js/bootstrap.js"></script>
+        <script src="/codecourses/scripts/script.js"></script>
+        <script src="script.js"></script>
+
     </body>
 </html>
--- tutorials/index.php
+++ tutorials/index.php
@@ -2,114 +2,198 @@
 <html>
     <head>
         <title>CodeCourses</title>
-        <link rel="stylesheet" href="css/bootstrap.css">
-        <link rel="stylesheet" href="css/style.css">
+        
+        <link rel="stylesheet" href = "/codecourses/assets/bootstrap-4.1.3-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="style.css">
 
+        <link rel = "script"   href = "/codecourses/assets/bootstrap-4.1.3-dist/js/bootstrap.min.js">
+        <script src="script.js"></script>
+       
     </head>
     <body>
        
         <!--include navigation bar from a preset php file-->
         <?php require $_SERVER['DOCUMENT_ROOT'] . "/codecourses/navbar_control.php";?>
-
         
-        <!-- Home Section -->
-        <div class="row-2">
-            <div class="col-6">
-                <div class="videoContainer">
-                     <iframe width="800" height="500" 
-                        src="https://www.youtube.com/embed/Syx2qDjj7TE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-                    </iframe>
-                </div>
-            </div>
-            <!-- Lessons -->
-            <div class="col-6">
-                <div class="lessons">
-                    <a href="###">Primes</a>
-                    <br><br><br>
-                    <a href="###">Factorization </a>
-                    <br><br><br>
-                    <a href="###">Factorial</a>
-                    <br><br><br>
-                    <a href="###">Fib, GCD, LCM, Pow</a>
-                    <br><br><br>
-                    <a href="###">Extended Euclidean algorithm</a>
-                    <br><br><br>
-                    <a href="###">Diophantine equation</a>
-                    <br><br><br>
-                    <a href="###">Modular multiplicative inverse</a>
-                    <br><br><br>
-                    <a href="###">Modular Arithmetic Apps</a>
-                    <br><br><br>
-                    <a href="###">Chinese Remainder Theorem</a>
-                    <br><br><br>
-                </div>
-            </div>
-        </div>
-        
-
-        <!-- Accordion Collapse -->
-        
-        <div class="accordion" id="accordionExample">
-            <!-- Resources -->
-            <div class="card">
-                <div class="card-header" id="headingOne">
-                    <h5 class="mb-0">
-                        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"
-                                style>
-                            Resources
-                        </button>
-                    </h5>
+        <div class="container">
+            <div class="row">
+                <div class="col-8">
+                    <!-- VideoContainer -->        
+                    <div class="videooContainer">
+                         <iframe width="750" height="500" 
+                             src="https://www.youtube.com/embed/RRouhxZJKak" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+                        </iframe>
+                    </div>
                 </div>
                 
-                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-                    <div class="card-body">
-                        <br> 
-                        <a href="###" target="_blank">Article 1</a>
-                        <br><br>
-                        <a href="###" target="_blank">Article 2</a>
-                        <br><br>
-                        <a href="###" target="_blank">Article 3</a>
-                        <br><br>
+                <div class="col-4">
+                    <!-- Accordion Collapse -->
+                    <div class="accordion" id="accordionExample">
+                        <!-- Beginner section -->
+                        <div class="card">
+                            <div class="card-header" id="headingOne">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"
+                                            style>
+                                        BEGINNERS
+                                    </button>
+                                </h5>
+                            </div>
+
+                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                                <div class="card-body">
+                                    <br> 
+                                    <a href="../TutorialsPage/index.html" target="_blank">1- Data Types</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">2- Operators</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">3- Array</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">4- String</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">5- Loop</a>
+                                    <br><br>    
+                                    <a href="###" target="_blank">6- Function</a>
+                                    <br><br>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- intermediate section -->
+                        <div class="card">
+                            <div class="card-header" id="headingTwo">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        INTERMEDIATE
+                                    </button>
+                                </h5>
+                            </div>
+
+                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                                <div class="card-body">
+                                    <br> 
+                                    <a href="###" target="_blank">1- Vector</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">2- List</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">3- Deque</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">4- Queue</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">5- Priority Queue</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">6- Stack</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">7- Set</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">8- Map </a>
+                                    <br><br>
+                                    <a href="###" target="_blank">9- Binary Search</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">10- Quick Sort</a>
+                                    <br><br>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Intermediate section -->
+                        <div class="card">
+                            <div class="card-header" id="headingThree">
+                              <h5 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                    ADVANCED
+                                </button>
+                              </h5>
+                            </div>
+
+                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                                <div class="card-body">
+                                    <br> 
+                                    <a href="###" target="_blank">1- Backtracking1</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">2- Backtracking2</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">3- Backtracking3</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">4- Backtracking4</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">5- Backtracking5</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">6- Convex Hull</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">7- Johnson’s algorithm</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">8- Huffman Coding | Greedy Algo</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">9- Minimum Partition</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">10- Optimal Binary Search Tree</a>
+                                    <br><br>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            
-            <!-- Problems -->
-            <div class="card">
-                <div class="card-header" id="headingTwo">
-                    <h5 class="mb-0">
-                        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                            Problems
-                        </button>
-                    </h5>
-                </div>
+            <!--Resources and Problems Section -->
+            <div class="row">
+                <div class="col-6">
+                    <!-- Accordion Collapse -->
+                    <div class="accordion" id="accordionExample">
+                        <!-- Resources -->
+                        <div class="card">
+                            <div class="card-header" id="headingOne">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"
+                                            style>
+                                        Resources
+                                    </button>
+                                </h5>
+                            </div>
 
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                    <div class="card-body">
-                        <br> 
-                        <a href="###" target="_blank">Problem 1</a>
-                        <br><br>
-                        <a href="###" target="_blank">Problem 2</a>
-                        <br><br>
-                        <a href="###" target="_blank">Problem 3</a>
-                        <br><br>
-                        <a href="###" target="_blank">Problem 4</a>
-                        <br><br>
-                        <a href="###" target="_blank">Problem 5</a>
-                        <br><br>
+                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                                <div class="card-body">
+                                    <br> 
+                                    <a href="https://www.geeksforgeeks.org/c-data-types/" target="_blank">Article 1</a>
+                                    <br><br>
+                                    <a href="https://www.tutorialspoint.com/cplusplus/cpp_data_types.htm" target="_blank">Article 2</a>
+                                    <br><br>
+                                    <a href="http://www.cplusplus.com/doc/tutorial/variables/" target="_blank">Article 3</a>
+                                    <br><br>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Problems -->
+                        <div class="card">
+                            <div class="card-header" id="headingTwo">
+                                <h5 class="mb-0">
+                                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        Problems
+                                    </button>
+                                </h5>
+                            </div>
+
+                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                                <div class="card-body">
+                                    <br> 
+                                    <a href="###" target="_blank">Problem 1</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">Problem 2</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">Problem 3</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">Problem 4</a>
+                                    <br><br>
+                                    <a href="###" target="_blank">Problem 5</a>
+                                    <br><br>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            
-        
-        </div>
-        
-        
-
-        <!-- Footer Section-->
-        <div id="footer">
-            &copy 2018 CodeCourses.com | All Rights Reserved
-        </div>        
+        </div>      
 
         <script src="js/jQuery.js"></script>
         <script src="js/bootstrap.js"></script>
