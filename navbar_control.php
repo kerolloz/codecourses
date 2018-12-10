34253da05e41029fa9327f3216eb3a3b54cdc539
kerolloz <kerolloz@yahoo.com>
2018-12-10 07:29:37 +0200

    fix nav bar tabs

--- navbar_control.php
+++ navbar_control.php
@@ -5,7 +5,7 @@
          id = "imgMobile">
     <ul id = "dropdownClick" class = "nav-left">
         <li><a href="/codecourses">Home</a></li>
-        <li><a href=<?= "/codecourses/problemset/" ?>>Problem Set</a></li>
+        <li><a href="/codecourses/problemset/">Problem Set</a></li>
         <li><a href="/codecourses/roadmap/">Road Map</a></li>
         <li><a href="/codecourses/contests/">Contests</a></li>
         <?php 
@@ -14,12 +14,11 @@
             session_start();
         if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true):
         ?>
-        <li class="nav-right"><a href="/codecourses/back/logout.php" >logout</a></li>
+        <li class="nav-right"><a href="/codecourses/back/logout.php" >Logout</a></li>
         <?php 
         else:
         ?>
-        <li class="nav-right"><a href="/codecourses/signin/">Sign In</a></li>
-        <li class="nav-right"><a href="/codecourses/signup/">Sign Up</a></li>
+        <li class="nav-right"><a href="/codecourses/authentication/">Authentication</a></li>
         <?php
         endif;
         ?>
