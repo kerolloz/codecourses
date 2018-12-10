af9c1f78685063e12d21ed54cb63f5cb25dd319b
kerolloz <kerolloz@yahoo.com>
2018-12-10 05:46:48 +0200

    fix style in authentication page

--- authentication/index.php
+++ authentication/index.php
@@ -8,8 +8,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <!--BS CSS-->
-        <link rel="stylesheet" href="assets/bootstrap-4.1.3-dist/css/bootstrap-grid.min.css">
-        <link rel="stylesheet" href="assets/bootstrap-4.1.3-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../assets/bootstrap-4.1.3-dist/css/bootstrap-grid.min.css">
+        <link rel="stylesheet" href="../assets/bootstrap-4.1.3-dist/css/bootstrap.min.css">
 
         <!--Google Fonts-->
         <link href="https://fonts.googleapis.com/css?family=Text+Me+One" rel="stylesheet"> 
@@ -21,7 +21,7 @@
     <body>
         <!--Navbar Section-->
         <nav>
-            <img src="assets/images/codeCourses.png" 
+            <img src="../assets/images/codeCourses.png" 
                     class = "nav-left"
                     id = "imgMobile">
         </nav>
