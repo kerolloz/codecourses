d28fdc47e7fa26d8367e9d25f1b395fa07e2ba98
yosefHesham <45171920+yosefHesham@users.noreply.github.com>
2018-12-12 22:45:23 +0200

    Add files via upload

---
+++ standings/standings.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Standings</title>
+        <link rel="stylesheet" href = "../assets/bootstrap-4.1.3-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="style.css">
+
+        <link rel = "script"   href = "../assets/bootstrap-4.1.3-dist/js/bootstrap.min.js">
+        <script src="script.js"></script>
+        <script src="/codecourses/scripts/script.js"></script>
+    </head>
+
+    <body>
+      	<div class ="tableWidth">
+      	<p> <bold> Standings </bold> </p>
+       <div class="table-responsive">
+
+    	  <table class ="table   table-striped  table-bordered" > 
+            <tr>
+            	<th> # </th>
+            	<th> Participant </th>
+            	<th> = </th>
+               	<th class="underline"> A </th>
+               	<th class ="underline"> B </th> 
+               	<th class ="underline"> C </th> 
+           	</tr>
+          <tr>
+              <td> 1 </td>
+              <td> Sample </td>
+              <td> 10 </td>
+                <td class ="score"> +3 </td>
+                <td class ="score"> +4 </td> 
+                <td class ="score"> +3 </td> 
+            </tr>
+
+
+           </table>
+       </div>
+      </div>
+
+</body>
+
+</html>
