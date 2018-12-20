3080bcabb14b4692dbf832d73721a65dc206ec34
Mohamed El-Raghy <elraghy8@gmail.com>
2018-12-20 14:41:00 +0200

    add python requirements

--- initiate_project/INSTALL_REQUIREMENTS.sh
+++ initiate_project/INSTALL_REQUIREMENTS.sh
@@ -1,5 +1,9 @@
 sudo apt-get update
 
+echo "Downloading Python3";
+sudo apt-get install python3 pip3 python3-pdfkit python3-pypdf2  wkhtmltopdf;
+pip3 install beautifulsoup pdfkit PyPDF2;
+
 sudo apt-get install \
    apt-transport-https \
    ca-certificates \
@@ -18,19 +22,25 @@ sudo add-apt-repository \
    stable"
 echo "SOLUTION: replace $(lsb_release -cs) with Bionic";
 
+echo "Installing docker";
 sudo apt-get update
-
 sudo apt-get install docker-ce
-
 # If you would like to use Docker as a non-root user
 # you should now consider adding your user to the “docker” group
 # with something like:
 sudo usermod -aG docker $USER
+sudo usermod -aG docker daemon
 # Remember to log out and back in for this to take effect!
 
-# download the judge image to the local docker repository
-docker pull kerolloz/codecourses_judge
-
+echo "Downloading XAMPP";
 wget https://www.apachefriends.org/xampp-files/7.2.12/xampp-linux-x64-7.2.12-0-installer.run
-
 sudo ./xampp-linux-x64-7.2.12-0-installer.run 
+
+echo "You should restart your device for changes to take effect!";
+echo "Do you want to restart now? (Y/N): ";
+read user_choice;
+if [ "$user_choice" = "Y" ]; then
+	reboot;
+else 
+	docker pull kerolloz/codecourses_judge
+fi
