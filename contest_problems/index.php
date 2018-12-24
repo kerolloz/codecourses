65e0ff7d4112ee2bcd1a023adbbf5be0a29758e2
AlaaOhman <alaa.othman4707@gmail.com>
2018-12-25 00:29:57 +0200

    Background fixed in contest_problems

--- contest_problems/index.php
+++ contest_problems/index.php
@@ -35,67 +35,66 @@ $sql = "SELECT * FROM problems WHERE contest_id=" . $contest_id;
         <!--include navigation bar from a preset php file-->
         <?php require $_SERVER['DOCUMENT_ROOT'] . "/codecourses/navbar_control.php";?>
 
-        <br> <br> <br> <br>
-
         <!-- Problems In Contest Section -->
-<div class = "tableWidth">
-      	
-       <div class="table-responsive">
+		<div class="color">
+			<div class = "tableWidth">
+					
+				<div class="table-responsive">
 
-    	  <table class ="table   table-striped  table-bordered" > 
-   	         
-            <tr>
-            	<th> # </th>
-               	<th> Name </th>
-               	<th> <img src="/codecourses/assets/images/user.png"> </th>
-               	<th> <img src="/codecourses/assets/images/ok.png" > </th> 
-           	</tr>
-           	<?php 
-      			$result = $conn->query($sql);
+					<table class ="table   table-striped  table-bordered" > 
+						
+						<tr>
+							<th> # </th>
+							<th> Name </th>
+							<th> <img src="/codecourses/assets/images/user.png"> </th>
+							<th> <img src="/codecourses/assets/images/ok.png" > </th> 
+						</tr>
+						<?php 
+							$result = $conn->query($sql);
 
-      			if ($result->num_rows > 0) :
-      			    // output data of each row
-      			    $problem_character = "A";
-      			    while($row = $result->fetch_assoc()) :
-      			?>
-			    	<tr>
-			    		<td> <?= $problem_character++ ?> </td>
-		                <td> <a  href="/codecourses/problem?id=<?= $row['problem_id'] ?>"> <?= $row['name'] ?> </a> </td>
-		                <td> <?= $row['number_of_solvers'] ?>  </td>
-		                <td> no  </td>
-		            </tr>
-			<?php
-				endwhile;
+							if ($result->num_rows > 0) :
+								// output data of each row
+								$problem_character = "A";
+								while($row = $result->fetch_assoc()) :
+							?>
+								<tr>
+									<td> <?= $problem_character++ ?> </td>
+									<td> <a  href="/codecourses/problem?id=<?= $row['problem_id'] ?>"> <?= $row['name'] ?> </a> </td>
+									<td> <?= $row['number_of_solvers'] ?>  </td>
+									<td> no  </td>
+								</tr>
+						<?php
+							endwhile;
 
-			else: 
-			    echo "<pre><br>NO PROBLEMS IN THIS CONTEST</pre>";
-			endif;
-			$conn->close();
-            ?>
+						else: 
+							echo "<pre><br>NO PROBLEMS IN THIS CONTEST</pre>";
+						endif;
+						$conn->close();
+						?>
 
 
-    	</table>
+					</table>
 
-	  </div>
+				</div>
 
-	<div class = "countDownTable">
+				<div class = "countDownTable">
 
-	    <table class ="table table-bordered table-striped">
+					<table class ="table table-bordered table-striped">
 
-    		<tr>
-    			<th> <span class="badge badge-danger"> CountDown !</span> </th> 
-    		</tr>
+						<tr>
+							<th> <span class="badge badge-danger"> CountDown !</span> </th> 
+						</tr>
 
-    		<tr>
-    			<td> <p id="countDown"> </p> </td>
-    		</tr>
+						<tr>
+							<td> <p id="countDown"> </p> </td>
+						</tr>
 
-    	</table>
-	</div>
+					</table>
+				</div>
 
-</div>
-
-<div id="footer">
+			</div>
+		</div>
+		<div id="footer">
             &copy 2018 CodeCourses.com | All Rights Reserved
         </div>        
 
