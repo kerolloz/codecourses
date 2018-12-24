76d0ddfb29ba987d93febeeaedd49deba123aa63
kerolloz <kerolloz@yahoo.com>
2018-12-25 00:20:27 +0200

    fix problem in contest page footer and background

--- contest_problems/index.php
+++ contest_problems/index.php
@@ -22,6 +22,8 @@ $sql = "SELECT * FROM problems WHERE contest_id=" . $contest_id;
     <head>
         <title>ProblemsInContest</title>
         <link rel="stylesheet" href = "/codecourses/assets/bootstrap-4.1.3-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../styles/style.css">
+
         <link rel="stylesheet" href="style.css">
 
         <link rel = "script"   href = "/codecourses/assets/bootstrap-4.1.3-dist/js/bootstrap.min.js">
