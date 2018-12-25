75ab4bd3201a4603d44ce64b0d4b22e68aa5d604
kerolloz <kerolloz@yahoo.com>
2018-12-25 05:32:22 +0200

    fix db connection

--- problemset/index.php
+++ problemset/index.php
@@ -1,15 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ojDB";
+
+
+require '../back/database_connection.php';
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+$conn = get_sql_connection();
 
 $sql = "SELECT * FROM problems";
 
