a61d13181209569af45c42e2a5497f93e1f11e67
kerolloz <kerolloz@yahoo.com>
2018-12-25 06:41:09 +0200

    user should be logged in to be able to submit

--- problem/index.php
+++ problem/index.php
@@ -1,7 +1,13 @@
 <?php
 $errors_array = [];
 if(isset($_POST['submit'])){
+  if(!isset($_SESSION))
+      session_start();
+  if(isset($_SESSION['is_logged_in']) && $_SESSION['is_logged_in'] == true):
     require '../judge_tester/process_submission.php';
+  else:
+      header("location: ../authentication/");
+  endif;
 }
 ?>
 
@@ -13,20 +19,20 @@ if(isset($_POST['submit'])){
         <link rel="stylesheet" href = "../assets/bootstrap-4.1.3-dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="../styles/style.css">
         <link rel="stylesheet" href="style.css">
-   
+
     </head>
     <body>
-       
+
         <!--include navigation bar from a preset php file-->
         <?php require $_SERVER['DOCUMENT_ROOT'] . "/codecourses/navbar_control.php";?>
-        
+
         <div class="color">
             <div class="container">
                 <div class="row">
                     <div class="col">
                         <!--Problem pdf -->
-                        <?php 
-                        $problem_id = filter_var($_GET['id'], FILTER_VALIDATE_INT, FILTER_NULL_ON_FAILURE); 
+                        <?php
+                        $problem_id = filter_var($_GET['id'], FILTER_VALIDATE_INT, FILTER_NULL_ON_FAILURE);
                         if(!$problem_id)
                             header("location: /codecourses/errors/404.html");
                         ?>
@@ -34,7 +40,7 @@ if(isset($_POST['submit'])){
                         <br><br><br>
                     </div>
                 </div>
-                
+
                 <div class="row">
                     <div class="col">
                         <div class="showsubmit">
@@ -42,7 +48,7 @@ if(isset($_POST['submit'])){
                             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
                             SUBMIT
                             </button>
-                            
+
                             <!-- Modal -->
                             <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="demo model" aria-hidden="true">
                                 <div class="modal-dialog" role="document">
@@ -53,7 +59,7 @@ if(isset($_POST['submit'])){
                                             <span aria-hidden="true">&times;</span>
                                             </button>
                                         </div>
-                                            
+
                                         <div class="modal-body">
                                             <div class="row">
                                                 <div class="col-4">
@@ -70,7 +76,7 @@ if(isset($_POST['submit'])){
                                                             <option value="5"> Clang++17 Diagnostics</option>
                                                             <option value="6">OpenCobol 1.0</option>
                                                             <option value="7">C# Mono 5</option>
-                                                            <option value="8">D DMD32 v2.079.0</option> 
+                                                            <option value="8">D DMD32 v2.079.0</option>
                                                             <option value="9">Go 1.11</option>
                                                             <option value="10">Haskell GHC 7.8.3</option>
                                                             <option value="11">Java 1.8.0_162</option>
@@ -86,7 +92,7 @@ if(isset($_POST['submit'])){
                                                     <br><br><br>
                                                 </div>
                                             </div>
-                                            
+
                                             <div class="row">
                                                 <div class="col-4">
                                                     <!--Source Code Section -->
@@ -105,7 +111,7 @@ if(isset($_POST['submit'])){
                                                             <div class="modal-footer">
                                                                 <button id="resbtn" type="submit" class="btn btn-primary" name="submit">
                                                                     SUBMIT
-                                                                </button>                                                   
+                                                                </button>
                                                             </div>
                                                         </form>
                                                     </div>
@@ -118,7 +124,7 @@ if(isset($_POST['submit'])){
                         </div>
                     </div>
                 </div>
-                
+
                 <!--3 Models ACC, WA, TLE -->
                 <!-- 1 -->
 
@@ -128,7 +134,7 @@ if(isset($_POST['submit'])){
                         <span class="closeRes">&times;</span>
                         <h3 id="judgeResult">Please Wait!</h3>
                     </div>
-                </div> 
+                </div>
             </div>
 
             <div id="footer">
@@ -138,7 +144,7 @@ if(isset($_POST['submit'])){
 
       <?php
 if (isset($return_value)):
-    
+
 switch ($return_value) {
     case 0:
     echo '<script>
@@ -180,7 +186,7 @@ endif;
 $return_value = null;
 ?>
         </div>
-        
+
         <script src="../assets/bootstrap-4.1.3-dist/js/jQuery.js"></script>
         <script src="../assets/bootstrap-4.1.3-dist/js/bootstrap.js"></script>
         <script src="../scripts/script.js"></script>
