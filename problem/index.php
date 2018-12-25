d85dc2352aa56647477193c2ee94b26f9c7120e8
AlaaOhman <alaa.othman4707@gmail.com>
2018-12-25 02:45:20 +0200

    fixed problem

--- problem/index.php
+++ problem/index.php
@@ -10,7 +10,7 @@ if(isset($_POST['submit'])){
     <head>
         <title>Problem</title>
 
-        <link rel="stylesheet" href = "../assets/bootstrap-4.1.3-dist/css/bootstrap.css">
+        <link rel="stylesheet" href = "../assets/bootstrap-4.1.3-dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="../styles/style.css">
         <link rel="stylesheet" href="style.css">
    
@@ -20,113 +20,121 @@ if(isset($_POST['submit'])){
         <!--include navigation bar from a preset php file-->
         <?php require $_SERVER['DOCUMENT_ROOT'] . "/codecourses/navbar_control.php";?>
         
-        <br>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <!--Problem pdf -->
-                    <?php 
-                    $problem_id = filter_var($_GET['id'], FILTER_VALIDATE_INT, FILTER_NULL_ON_FAILURE); 
-                    if(!$problem_id)
-                        header("location: /codecourses/errors/404.html");
-                    ?>
-                    <embed class="pdfContainer" src="/codecourses/problems_db/<?= $problem_id ?>/problem.pdf"> <!--src=#problem pdf-->
-                    <br><br><br>
+        <div class="color">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <!--Problem pdf -->
+                        <?php 
+                        $problem_id = filter_var($_GET['id'], FILTER_VALIDATE_INT, FILTER_NULL_ON_FAILURE); 
+                        if(!$problem_id)
+                            header("location: /codecourses/errors/404.html");
+                        ?>
+                        <embed class="pdfContainer" src="/codecourses/problems_db/<?= $problem_id ?>/problem.pdf"> <!--src=#problem pdf-->
+                        <br><br><br>
+                    </div>
                 </div>
-            </div>
-            
-            <div class="row">
-                <div class="col">
-                    <div class="showsubmit">
-                        <!-- Button trigger modal -->
-                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
-                          SUBMIT
-                        </button>
-                        <!-- Modal -->
-                        <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="demo model" aria-hidden="true">
-                          <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                              <div class="modal-header">
-                                <h4 class="modal-title" id="exampleModalLongTitle">Submit solution</h4>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                  <span aria-hidden="true">&times;</span>
-                                </button>
-                              </div>
-                                
-                            <div class="modal-body">
-                                <div class="row">
-                                    <div class="col-4">
-                                        <div class="lang"><h5>Language:</h5></div>
-                                    </div>
-                                    <div class="col-8">
-                                        <div class="user-select">
-                                            <select name="compiler">
-                                                Clang++17 Diagnostics
-                                                <option value="1">GNU G++17 7.3.0</option>
-                                                <option value="2">PHP 7.0.12</option>
-                                                <option value="3">Rust 1.26</option>
-                                                <option value="4">ActiveTcl 8.5</option>
-                                                <option value="5"> Clang++17 Diagnostics</option>
-                                                <option value="6">OpenCobol 1.0</option>
-                                                <option value="7">C# Mono 5</option>
-                                                <option value="8">D DMD32 v2.079.0</option> 
-                                                <option value="9">Go 1.11</option>
-                                                <option value="10">Haskell GHC 7.8.3</option>
-                                                <option value="11">Java 1.8.0_162</option>
-                                                <option value="12">Kotlin 1.2</option>
-                                                <option value="13">JavaScript V8 4.8.0</option>
-                                                <option value="14">Node.js 6.9.1</option>
-                                                <option value="15">Microsoft Q#</option>
-                                                <option value="16">Ruby 2.0.0p645</option>
-                                                <option value="17">PascalABC.NET 2</option>
-                                                <option value="18">Python 3.6</option>
-                                            </select>
+                
+                <div class="row">
+                    <div class="col">
+                        <div class="showsubmit">
+                            <!-- Button trigger modal -->
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+                            SUBMIT
+                            </button>
+                            
+                            <!-- Modal -->
+                            <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="demo model" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h4 class="modal-title" id="exampleModalLongTitle">Submit solution</h4>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                            </button>
                                         </div>
-                                        <br><br><br>
-                                    </div>
-                                </div>
-                                
-                                <div class="row">
-                                    <div class="col-4">
-                                        <!--Source Code Section -->
-                                        <div class="sors"><h5>Source Code:</h5></div>
-                                    </div>
-                                    <div class="col-8">
-                                        <div class="sendSubmit">
-                                            <form action="" method="post" enctype="multipart/form-data">
-                                                <textarea name="code" rows="20"><?php if($errors_array){ echo htmlentities($code);}?></textarea>
-                                                <br>
-                                                <br>
-                                                <input type="hidden" name="problem_id" value="<?= $_GET['id'] ?>">
-                                                <input type="file" name="my_file">
-                                                <br>
-                                                <br>
-                                                <div class="modal-footer">
-                                                    <button id="resbtn" type="submit" class="btn btn-primary" name="submit">
-                                                        SUBMIT
-                                                    </button>                                                   
+                                            
+                                        <div class="modal-body">
+                                            <div class="row">
+                                                <div class="col-4">
+                                                    <div class="lang"><h5>Language:</h5></div>
                                                 </div>
-                                            </form>
+                                                <div class="col-8">
+                                                    <div class="user-select">
+                                                        <select name="compiler">
+                                                            Clang++17 Diagnostics
+                                                            <option value="1">GNU G++17 7.3.0</option>
+                                                            <option value="2">PHP 7.0.12</option>
+                                                            <option value="3">Rust 1.26</option>
+                                                            <option value="4">ActiveTcl 8.5</option>
+                                                            <option value="5"> Clang++17 Diagnostics</option>
+                                                            <option value="6">OpenCobol 1.0</option>
+                                                            <option value="7">C# Mono 5</option>
+                                                            <option value="8">D DMD32 v2.079.0</option> 
+                                                            <option value="9">Go 1.11</option>
+                                                            <option value="10">Haskell GHC 7.8.3</option>
+                                                            <option value="11">Java 1.8.0_162</option>
+                                                            <option value="12">Kotlin 1.2</option>
+                                                            <option value="13">JavaScript V8 4.8.0</option>
+                                                            <option value="14">Node.js 6.9.1</option>
+                                                            <option value="15">Microsoft Q#</option>
+                                                            <option value="16">Ruby 2.0.0p645</option>
+                                                            <option value="17">PascalABC.NET 2</option>
+                                                            <option value="18">Python 3.6</option>
+                                                        </select>
+                                                    </div>
+                                                    <br><br><br>
+                                                </div>
+                                            </div>
+                                            
+                                            <div class="row">
+                                                <div class="col-4">
+                                                    <!--Source Code Section -->
+                                                    <div class="sors"><h5>Source Code:</h5></div>
+                                                </div>
+                                                <div class="col-8">
+                                                    <div class="sendSubmit">
+                                                        <form action="" method="post" enctype="multipart/form-data">
+                                                            <textarea name="code" rows="20"><?php if($errors_array){ echo htmlentities($code);}?></textarea>
+                                                            <br>
+                                                            <br>
+                                                            <input type="hidden" name="problem_id" value="<?= $_GET['id'] ?>">
+                                                            <input type="file" name="my_file">
+                                                            <br>
+                                                            <br>
+                                                            <div class="modal-footer">
+                                                                <button id="resbtn" type="submit" class="btn btn-primary" name="submit">
+                                                                    SUBMIT
+                                                                </button>                                                   
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            </div>
-                          </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <!--3 Models ACC, WA, TLE -->
-            <!-- 1 -->
+                
+                <!--3 Models ACC, WA, TLE -->
+                <!-- 1 -->
 
-            <div id="res" class="finalResultClass">
-              <!-- Modal content -->
-              <div class="resultMod">
-                <span class="closeRes">&times;</span>
-                <h3 id="judgeResult">Please Wait!</h3>
-              </div>
+                <div id="res" class="finalResultClass">
+                    <!-- Modal content -->
+                    <div class="resultMod">
+                        <span class="closeRes">&times;</span>
+                        <h3 id="judgeResult">Please Wait!</h3>
+                    </div>
+                </div> 
             </div>
+
+            <div id="footer">
+                &copy 2018 CodeCourses.com | All Rights Reserved
+            </div>
+        </div>
 
       <?php
 if (isset($return_value)):
@@ -173,7 +181,6 @@ $return_value = null;
 ?>
         </div>
         
-        <br>
         <script src="../assets/bootstrap-4.1.3-dist/js/jQuery.js"></script>
         <script src="../assets/bootstrap-4.1.3-dist/js/bootstrap.js"></script>
         <script src="../scripts/script.js"></script>
