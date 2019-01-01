ffbd9aecd0ae3af6b34393d0cbae2c8fe6ebe716
kerolloz <kerolloz@yahoo.com>
2019-01-01 12:08:37 +0200

    table styling

--- admin_dashboard/index.php
+++ admin_dashboard/index.php
@@ -21,73 +21,89 @@ $select_contests_sql_stmnt_string = "SELECT * FROM contests";
 
             <!-- MAIN CONTENT-->
             <div class="main-content">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <!-- DATA TABLE -->
+                                <div class="table-data__tool">
+                                    <div class="table-data__tool-left">
+                                        <h3 class="title-5">contests table</h3>
+                                    </div>
+                                    <div class="table-data__tool-right">
+                                        <button class="au-btn au-btn-icon au-btn--green au-btn--small">
+                                            <i class="zmdi zmdi-plus"></i>add contest</button>
+                                    </div>
+                                </div>
+                                <div class="table-responsive table--no-card">
+                                    <table class="table table-borderless table-striped table-earning table-data2 table-data3 black-table">
+                                        <thead>
+                                            <tr>
+                                                <th>Contest ID</th>
+                                                <th>Name</th>
+                                                <th>Date</th>
+                                                <th>Duration</th>
+                                                <th>Setter</th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                                                $result = $connection->query($select_contests_sql_stmnt_string);
+                                                                if (isset($result->num_rows) && $result->num_rows) {
+                                                                    while ($row = $result->fetch_assoc()):
 
-                <div class="table-responsive table-responsive-data2">
-                    <table class="table table-data2">
-                        <thead>
-                            <tr>
-                            <th>Contest ID</th>
-                            <th>Name</th>
-                            <th>Date</th>
-                            <th>Duration</th>
-                            <th>Setter</th>
-                            <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                                    $result = $connection->query($select_contests_sql_stmnt_string);
-                                    if (isset($result->num_rows) && $result->num_rows) {
-                                        while ($row = $result->fetch_assoc()):
+                                                                ?>
+                                            <tr class="tr-shadow <?= $row['contest_id'] ?>">
 
-                                    ?>
-                                <tr class="tr-shadow <?= $row['contest_id'] ?>">
-
-                                    <td>
-                                        <?= $row['contest_id'] ?>
-                                    </td>
-                                    <td>
-                                        <a href="../contest_problems/?id=<?= $row['contest_id'] ?>">
-                                            <?= $row['name'] ?>
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <?= $row['date'] ?>
-                                    </td>
-                                    <td>
-                                        <?= $row['length'] ?>
-                                    </td>
-                                    <td>
-                                        <?= $row['setter'] ?>
-                                    </td>
-                                    <td>
-                                        <div class="table-data-feature">
-                                            <button class="item" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit">
-                                                <i class="zmdi zmdi-edit"></i>
-                                            </button>
-                                            <button id="<?= $row['contest_id'] ?>" class="item delete-class" data-toggle="tooltip" type="button" data-placement="top" title="" data-original-title="Delete">
-                                                <i class="zmdi zmdi-delete"></i>
-                                            </button>
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr class="spacer"></tr>
+                                                <td>
+                                                    <?= $row['contest_id'] ?>
+                                                </td>
+                                                <td>
+                                                    <a href="../contest_problems/?id=<?= $row['contest_id'] ?>">
+                                                        <?= $row['name'] ?>
+                                                    </a>
+                                                </td>
+                                                <td>
+                                                    <?= $row['date'] ?>
+                                                </td>
+                                                <td>
+                                                    <?= $row['length'] ?>
+                                                </td>
+                                                <td>
+                                                    <?= $row['setter'] ?>
+                                                </td>
+                                                <td>
+                                                    <div class="table-data-feature">
+                                                        <button class="item" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit">
+                                                            <i class="zmdi zmdi-edit"></i>
+                                                        </button>
+                                                        <button id="<?= $row['contest_id'] ?>" class="item delete-class" data-toggle="tooltip" type="button" data-placement="top" title="" data-original-title="Delete">
+                                                            <i class="zmdi zmdi-delete"></i>
+                                                        </button>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            <tr class="spacer"></tr>
 
 
-                            <?php  endwhile;
-                                    } else {
-                                        echo "<tr class='tr-shadow'>
-                                <td colspan='6'>
-                                NO AVAILABLE CONTESTS
-                                </td>
-                                </tr>";
-                                    } ?>
+                                            <?php  endwhile;
+                                                                } else {
+                                                                    echo "<tr class='tr-shadow'>
+                                                            <td colspan='6'>
+                                                            NO AVAILABLE CONTESTS
+                                                            </td>
+                                                            </tr>";
+                                                                } ?>
 
-                        </tbody>
-                    </table>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <!-- END DATA TABLE -->
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-
 
             </div>
         </div>
@@ -99,4 +115,3 @@ $select_contests_sql_stmnt_string = "SELECT * FROM contests";
 </body>
 
 </html>
-<!-- end document-->
