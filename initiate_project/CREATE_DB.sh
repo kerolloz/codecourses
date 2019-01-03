4802976015b1ff7d4760dd3f116ab2848d12b379
kerolloz <kerolloz@yahoo.com>
2019-01-03 22:10:14 +0200

    beautify sh scripts, rename RUN_THIS_FILE -> CREATE_DB

--- initiate_project/CREATE_DB.sh
+++ initiate_project/CREATE_DB.sh
@@ -8,15 +8,15 @@ echo "[-]Initiating your project!\n";
 echo "[-]Activating Apache and MySQL servers";
 
 start_xampp startapache
-start_xampp startmysql 
+start_xampp startmysql
 
 echo "[-]Please wait 5 Seconds";
 
-sleep 5; # wait untill the servers run correctly 
+sleep 5; # wait untill the servers run correctly
 
 echo "[-]Running db_creation.php";
 
-/opt/lampp/bin/php ./db_creation.php; 
+/opt/lampp/bin/php ./db_creation.php;
 
 echo "[-]Opening Firefox localhost/codecourses";
 
--- initiate_project/INSTALL_REQUIREMENTS.sh
+++ initiate_project/INSTALL_REQUIREMENTS.sh
@@ -5,21 +5,21 @@ sudo apt-get install python3 pip3 python3-pdfkit python3-pypdf2  wkhtmltopdf;
 pip3 install beautifulsoup4 pdfkit PyPDF2 flask;
 
 sudo apt-get install \
-   apt-transport-https \
-   ca-certificates \
-   curl \
-   software-properties-common \
-   wget
-   
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common \
+    wget
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo apt-key fingerprint 0EBFCD88
 
 echo "WARNING: Ubuntu 18.10 users may have problems with the following command";
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
-   stable"
+    stable"
 echo "SOLUTION: replace $(lsb_release -cs) with Bionic";
 
 echo "Installing docker";
@@ -36,13 +36,13 @@ sudo docker pull kerolloz/codecourses_judge
 
 echo "Downloading XAMPP";
 wget https://www.apachefriends.org/xampp-files/7.2.12/xampp-linux-x64-7.2.12-0-installer.run
-sudo ./xampp-linux-x64-7.2.12-0-installer.run 
+sudo ./xampp-linux-x64-7.2.12-0-installer.run
 
 echo "You should restart your device for changes to take effect!";
 echo "Do you want to restart now? (Y/N): ";
 read user_choice;
 if [ "$user_choice" = "Y" ]; then
-	reboot;
-else 
-	echo "Docker Commands may not work properly!";
+    reboot;
+else
+    echo "Docker Commands may not work properly!";
 fi
