45725259c2038fe46a4ff6e22bec6362fee19492
kerolloz <kerolloz@yahoo.com>
2019-01-04 12:04:13 +0200

    todo comment

--- back/delete_contest.php
+++ back/delete_contest.php
@@ -3,6 +3,7 @@
 require 'database_connection.php';
 
 if (isset($_POST['contest_id'])) {
+    // TODO: Delete all associated problems
     $connection = get_sql_connection();
     if (delete_contest_by_id($connection, $_POST['contest_id'])) {
         echo "Deleted successfully";
