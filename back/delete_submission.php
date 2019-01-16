e62902f5b9751f5f1be1bb461b4ffad43e11b07c
Kerollos Magdy <kerolloz@yahoo.com>
2019-01-17 00:20:23 +0200

    code refactoring

--- back/delete_submission.php
+++ back/delete_submission.php
@@ -5,7 +5,7 @@ if(isset($_POST)){
     $connection = get_sql_connection();
     $is_all = $_POST['all'];
     // submission_id = null, delete all
-    if(delete_submissions_by_id($connection, null, $is_all)){
+    if(delete_submissions_by_submission_id($connection, null, $is_all)){
         echo "All submissions are deleted successfully";
     }else{
         echo "Something went wrong";
