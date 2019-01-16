e106c9caaec763af80823a1876cade630f5f3b2a
Kerollos Magdy <kerolloz@yahoo.com>
2019-01-17 00:20:57 +0200

    delete submissions before deleting problem

--- back/delete_problem.php
+++ back/delete_problem.php
@@ -4,10 +4,18 @@ require_once 'database_connection.php';
 
 if (isset($_POST['problem_id'])) {
     $connection = get_sql_connection();
-    if (delete_problem_by_id($connection, $_POST['problem_id'])) {
+
+    if(delete_submissions_by_problem_id($connection, $_POST['problem_id']))
+        echo "Associated submissions are deleted!";
+    else
+        echo "Couldn't delete associated submissions";
+
+    echo "\n";
+    
+    if (delete_problem_by_id($connection, $_POST['problem_id']))
         echo "Deleted successfully";
-    } else {
+    else
         echo "Something went wrong";
-    }
+
     close_sql_connection($connection);
 }
