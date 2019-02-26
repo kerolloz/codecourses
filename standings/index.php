806f833ec9de46c665fd0689e8378ba03b2364a6
kerollos <kerolloz@yahoo.com>
2019-02-26 06:10:01 +0200

    auto reload in submissions and standings

--- standings/index.php
+++ standings/index.php
@@ -10,6 +10,9 @@ $contest_id = filter_var($_GET['id'], FILTER_VALIDATE_INT, FILTER_NULL_ON_FAILUR
 if(!$contest_id)
     header("location: /codecourses/errors/404.html");
 
+$page = $_SERVER['PHP_SELF'] . "?id=$contest_id";
+$refresh_perioud = 3;
+
 ?>
 
 <!doctype html>
@@ -20,6 +23,7 @@ if(!$contest_id)
     <link rel="stylesheet" href="../assets/bootstrap-4.1.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/codecourses/styles/style.css">
     <link rel="stylesheet" href="style.css">
+    <meta http-equiv="refresh" content="<?= $refresh_perioud ?>;URL='<?= $page ?>'">
 
     <link rel="script" href="../assets/bootstrap-4.1.3-dist/js/bootstrap.min.js">
     <script src="../scripts/script.js"></script>
@@ -47,7 +51,7 @@ if(!$contest_id)
     								$problem_character = "A";
     								while($problem_row = $problems_result->fetch_assoc()) :
     							?>
-                        <th class="underline"><a href="#"><?= $problem_character++ ?></a></th>
+                        <th class="underline"><a href="../problem?id=<?= $problem_row['problem_id'] ?>"><?= $problem_character++ ?></a></th>
                         <?php
                     endwhile;
                 endif;
--- submissions/index.php
+++ submissions/index.php
@@ -15,6 +15,9 @@ INNER JOIN users ON submissions.user_id = users.user_id)
 ORDER BY submission_id DESC
 ";
 
+$page = $_SERVER['PHP_SELF'];
+$refresh_perioud = 3;
+
 ?>
 
 <!doctype html>
@@ -25,6 +28,7 @@ ORDER BY submission_id DESC
     <link rel="stylesheet" href="../assets/bootstrap-4.1.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/codecourses/styles/style.css">
     <link rel="stylesheet" href="style.css">
+    <meta http-equiv="refresh" content="<?= $refresh_perioud ?>;URL='<?= $page ?>'">
 
     <link rel="script" href="../assets/bootstrap-4.1.3-dist/js/bootstrap.min.js">
     <script src="../scripts/script.js"></script>
